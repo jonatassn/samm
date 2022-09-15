1899762f0a28251ba5fb898b4a3ddcc7121f2a49
Jonatas Silveira <48066050+jonatassn@users.noreply.github.com>
2022-09-15 00:55:47 -0300

    Implementado a classe Dao.java

--- src/br/com/jonatassn/samm/dao/Dao.java
+++ src/br/com/jonatassn/samm/dao/Dao.java
@@ -5,10 +5,25 @@
  */
 package br.com.jonatassn.samm.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * @author Jonatas.Silveira - Unespar <jonatas.silveira@unespar.edu.br>
  *
  */
 public class Dao {
-
+	private static EntityManager entityManager = null;
+	private static EntityManagerFactory entityManagerFactory = null;
+	
+	public static EntityManager getEntityManager() {
+		if(entityManagerFactory == null) {
+			entityManagerFactory = Persistence.createEntityManagerFactory("DS-SAMM-DEV");
+		}
+		if(entityManager == null) {
+			entityManager = entityManagerFactory.createEntityManager();
+		}
+		return entityManager;	
+	}
 }
