9b03ff7d8423c6afd03454fd727450aa69605f01
Jonatas Silveira <48066050+jonatassn@users.noreply.github.com>
2022-09-14 22:33:30 -0300

    Criado Classe para Autenticação

---
+++ src/br/com/jonatassn/samm/service/AuthenticationInit.java
@@ -0,0 +1,14 @@
+/*
+ * Universidade Estadual do Paraná - Unespar
+ * Núcleo de Tecnologia da Informação - NTI
+ * Copyright (c) 2022 - Todos os direitos reservados.
+ */
+package br.com.jonatassn.samm.service;
+
+/**
+ * @author Jonatas.Silveira - Unespar <jonatas.silveira@unespar.edu.br>
+ *
+ */
+public class AuthenticationInit {
+
+}
