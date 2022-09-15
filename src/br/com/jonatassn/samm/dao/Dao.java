ced25a4015c0fb6a18bdd0e45fe8be366936bf5e
Jonatas Silveira <48066050+jonatassn@users.noreply.github.com>
2022-09-14 22:29:43 -0300

    Classes iniciais criadas

---
+++ src/br/com/jonatassn/samm/dao/Dao.java
@@ -0,0 +1,14 @@
+/*
+ * Universidade Estadual do Paraná - Unespar
+ * Núcleo de Tecnologia da Informação - NTI
+ * Copyright (c) 2022 - Todos os direitos reservados.
+ */
+package br.com.jonatassn.samm.dao;
+
+/**
+ * @author Jonatas.Silveira - Unespar <jonatas.silveira@unespar.edu.br>
+ *
+ */
+public class Dao {
+
+}
